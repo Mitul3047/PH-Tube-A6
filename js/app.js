ea8c215e708ce5225c23b0977ff9d666abc8365f
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-01 01:05:17 +0600

    dynamic card added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const handleBlog = () => {
 }
 
 
-// catagory handler
+// catagories handler
 
 const handelcatagory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
@@ -16,17 +16,57 @@ const handelcatagory = async () => {
     data.data?.forEach((category) => {
         // console.log(category)
         const div = document.createElement("div")
-        div.innerHTML =`
-        <button  class = "btn rounded active:bg-[[#FF1F3D] active:text-white ">${category.category}</button>
+        div.innerHTML = `
+        <button  class = "btn rounded active:bg-[#FF1F3D] active:text-white" onclick ="handleCategoryButton('${category.category_id}')">${category.category}</button>
         `
         categoriesContainer.appendChild(div)
         //     `
         // <a class="tab" onclick ="handleTabNews('${category.category_id}')">${category.category_name}</a>  
         // `
+
         
-        // handleTabNews('01')
     })
 
 }
 
+// handle category button 
+
+const handleCategoryButton = async (categoryId) => {
+    // console.log(categoryId)
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+    const data = await response.json();
+    console.log(data.data)
+    // const tabNewsList = data.data
+    // console.log(tabNewsList)
+
+    const cardContainer = document.getElementById('card-container')
+      cardContainer.innerHTML='';
+    data.data?.forEach((categoryType) => {
+        console.log(categoryType)
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card  bg-base-100 shadow-xl">
+                <figure><img src="${categoryType?.thumbnail}" alt="thumblin" /></figure>
+                <div class="card-body">
+                  <div class=" flex justify-between items-center">
+                    <img src="${categoryType?.authors[0].profile_picture}" alt="profile picture" class="w-14 rounded-full">
+                    <h3>${categoryType?.title}</h3>
+                  </div>
+                  <div class="flex justify-between items-center">
+                    <h3>${categoryType?.authors[0].profile_name}</h3>
+                    <p> ${categoryType?.authors[0]?.verified}</p>
+                  </div>
+                  <h2>${categoryType?.others?.views}</h2>
+                </div>
+              </div>
+        `
+    
+        cardContainer.appendChild(div)
+    })
+
+}
+
+
+
+handleCategoryButton('1000')
 handelcatagory()
